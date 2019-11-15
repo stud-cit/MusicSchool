be139beb1703ced179bedbcbf34051f4eb3f4c88
Andrey <gorand.one@gmail.com>
2019-11-15 12:02:09 +0200

    fix map

--- resources/js/components/Other/ContactsComponent.vue
+++ resources/js/components/Other/ContactsComponent.vue
@@ -20,7 +20,7 @@
             </div>
 
         </div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1257.744630396848!2d34.79460276987388!3d50.91466584541763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4129022862ea422f%3A0x1620189a40515f9f!2z0YPQu9C40YbQsCDQl9Cw0YHRg9C80YHQutCw0Y8sIDEw0LAsINCh0YPQvNGLLCDQodGD0LzRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwgNDAwMDA!5e0!3m2!1sru!2sua!4v1573809282616!5m2!1sru!2sua" width="100%" height="600" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1257.744630396848!2d34.79460276987388!3d50.91466584541763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4129022862ea422f%3A0x1620189a40515f9f!2z0YPQu9C40YbQsCDQl9Cw0YHRg9C80YHQutCw0Y8sIDEw0LAsINCh0YPQvNGLLCDQodGD0LzRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwgNDAwMDA!5e0!3m2!1sru!2sua!4v1573809282616!5m2!1sru!2sua" width="100%"  frameborder="0" style="border:0;" allowfullscreen=""></iframe>
     </div>
 </template>
 
@@ -47,6 +47,7 @@
 </script>
 
 <style lang="sass" scoped>
-
+    .map
+        height: 780px
 
 </style>
