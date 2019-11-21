c766ed8917769dee944efaae0db624607d082597
Vadym <v.volin1488@gmail.com>
2019-11-21 14:02:23 +0200

    start introduction page

---
+++ resources/js/components/Introduction/IntroductionComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <PageTitleComponent></PageTitleComponent>
+        <b-container>
+            <b-col cols="12"></b-col>
+        </b-container>
+    </div>
+</template>
+
+<script>
+import PageTitleComponent from "../PageTitleComponent.vue";
+
+export default {
+    name: "IntroductionComponent"
+};
+</script>
+
+<style scoped></style>
