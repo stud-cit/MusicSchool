ce0db194815bec84031c0ba51ef3c9ab8dd342e4
doratoles <46978407+doratoles@users.noreply.github.com>
2019-11-21 12:11:28 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,55 @@
 */
 
 Route::get('/{any}', 'SpaController@index')->where('any', '.*');
+
+//Backend
+//Отделы
+Route::get('get-all-department', 'DepartmentController@getDepartments');
+Route::post('post-department', 'DepartmentController@postDepartments');
+Route::post('update-department/{id}/', 'DepartmentController@updateDepartments');
+
+Route::get('/teacher/{id}', 'DepartmentController@getTeachers');
+Route::get('/instrument/{id}', 'DepartmentController@getInstruments');
+
+Route::post('post-teacher', 'DepartmentController@postTeachers');
+Route::post('post-instrument', 'DepartmentController@postInstruments');
+Route::post('update-teacher/{id}/', 'DepartmentController@uupdateTeachers');
+Route::post('update-instrument/{id}/', 'DepartmentController@updateInstruments');
+
+Route::post('delete-department/{id}/', 'DepartmentController@deleteDepartments');
+Route::post('delete-teacher/{id}/', 'DepartmentController@deleteTeachers');
+Route::post('delete-instrument/{id}/', 'DepartmentController@deleteInstruments');
+
+//Документы
+Route::get('get-document', 'DocumentController@getDocument');
+Route::post('post-document', 'DocumentController@postDocument');
+
+//Меню
+Route::get('get-info', 'InfoController@getInfo');
+Route::post('post-info', 'InfoController@postInfo');
+Route::post('update-info/{id}/', 'InfoController@updateInfo');
+Route::post('delete-info/{id}/', 'InfoController@deleteInfo');
+
+//Вступ
+Route::get('get-intro', 'IntroController@getIntro');
+Route::post('post-intro', 'IntroController@postIntro');
+Route::post('update-intro/{id}/', 'IntroController@updateIntro');
+Route::post('delete-intro/{id}/', 'IntroController@deleteIntro');
+
+//Новости Достижения История
+Route::get('get-nas', 'NewsAchieveStoryController@getNewsAchieveStory');
+Route::get('get-news', 'NewsAchieveStoryController@getNews');
+Route::get('get-achieve', 'NewsAchieveStoryController@getAchieve');
+Route::get('get-story', 'NewsAchieveStoryController@getStory');
+
+Route::post('post-news', 'NewsAchieveStoryController@postNews');
+Route::post('post-achieve', 'NewsAchieveStoryController@postAchieve');
+Route::post('post-story', 'NewsAchieveStoryController@postStory');
+
+Route::post('update-news/{id}/', 'NewsAchieveStoryController@updateNews');
+Route::post('update-achieve/{id}/', 'NewsAchieveStoryController@updateAchieve');
+Route::post('update-story/{id}/', 'NewsAchieveStoryController@updateStory');
+
+Route::post('delete-news/{id}/', 'NewsAchieveStoryController@deleteNews');
+Route::post('delete-achieve/{id}/', 'NewsAchieveStoryController@deleteAchieve');
+Route::post('delete-story/{id}/', 'NewsAchieveStoryController@deleteStory');
