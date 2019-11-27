edffe6f838d3cc6b439d0a2741c79507bbe14698
doratoles <46978407+doratoles@users.noreply.github.com>
2019-11-27 12:16:02 +0200

    merge

--- database/seeds/InstrumentsTableSeeder.php
+++ database/seeds/InstrumentsTableSeeder.php
@@ -16,21 +16,21 @@ class InstrumentsTableSeeder extends Seeder
             'departments_id' => 1,
             'name_instruments' => 'Баян',
             'instruments_info' => 'Інформація про інструмент',
-            'photo' => '/site-files/bayan_photo.jpg'
+            'photo' => '/site-files/instruments.png'
         ]);
         DB::table('instruments')->insert([
             'instruments_id' => 2,
             'departments_id' => 2,
             'name_instruments' => 'Рояль',
             'instruments_info' => 'Інформація про інструмент',
-            'photo' => '/site-files/royal_photo.jpg'
+            'photo' => '/site-files/instruments.png'
         ]);
         DB::table('instruments')->insert([
             'instruments_id' => 3,
             'departments_id' => 3,
             'name_instruments' => 'Гітара',
             'instruments_info' => 'Інформація про інструмент',
-            'photo' => '/site-files/guitar_photo.jpg'
+            'photo' => '/site-files/instruments.png'
         ]);
     }
 }
