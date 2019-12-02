844d83b184a4e94d8d1a234c73ce16f06b17b7d0
SkyWalker1996x <chehuta1996@gmail.com>
2019-12-02 10:25:19 +0200

    add pagination to AchievementsComponent

--- resources/js/components/Other/AchievementsComponent.vue
+++ resources/js/components/Other/AchievementsComponent.vue
@@ -1,13 +1,128 @@
 <template>
+    <div>
+        <page-title title="досягнення" uptitle="важливі"></page-title>
+        <section class="history-section mt-50">
 
+            <b-container class="news-list">
+
+                <div class="news" v-for="i in paginateArray" :key="i.id">
+                    <div class="news-description">
+                        <h3 class="news-date">{{i.date}}</h3>
+                        <p class="news-text">{{i.title}}</p>
+                        <hr class="news-line">
+                    </div>
+                    <div class="news-img">
+                        <img :src="i.img" alt="" class="news-img">
+                    </div>
+                </div>
+            </b-container>
+
+            <paginate :items="achievements" @paginateArray="paginateArray = $event" perPage="6"></paginate>
+
+        </section>
+    </div>
 </template>
 
 <script>
+    import paginate from '../PaginationComponent';
+
     export default {
-        name: "AchievementsComponent"
+        name: "AchievementsComponent",
+        data() {
+            return {
+                achievements: [
+                    { id: 1, date: '01.01.19', title: '1 kek lol kek lol kek lol kek lol', img: "/img/man_scream.png"},
+                    { id: 2, date: '01.01.01', title: '2 heute ist goot', img: "/img/man_scream.png" },
+                    { id: 3, date: '01.01.18', title: 'abra kadabra', img: "/img/man_scream.png" },
+                    { id: 4, date: '01.01.15', title: 'peremoga', img: "/img/man_scream.png" },
+                    { id: 5, date: '01.01.16', title: 'ne mogu bez', img: "/img/man_scream.png" },
+                    { id: 6, date: '01.01.18', title: 'start rofl', img: "/img/man_scream.png" },
+                    { id: 7, date: '01.01.18', title: 'omg', img: "/img/man_scream.png" },
+                    { id: 8, date: '01.01.19', title: 'kek lol arbidol', img: "/img/man_scream.png" },
+                    { id: 9, date: '01.01.18', title: `peremogen'ka`, img: "/img/man_scream.png" },
+                    { id: 10, date: '01.01.18', title: `10`, img: "/img/man_scream.png" },
+                    { id: 11, date: '01.01.18', title: `11`, img: "/img/man_scream.png" },
+                ],
+                paginateArray: [],
+            }
+        },
+        computed: {
+
+        },
+        components: {
+            paginate
+        }
     }
 </script>
 
 <style scoped>
+    .news-list {
+        display: flex;
+        flex-wrap: wrap;
+    }
 
+    .news {
+        position: relative;
+        width: 30%;
+        margin: 15px 15px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        text-align: center;
+        overflow: hidden;
+        box-shadow: 5px 5px 25px rgba(0, 0, 0, 0.16);
+        transition: background .2s linear;
+    }
+
+    .news-description {
+        padding: 50px 10px 10px;
+        max-width: 190px;
+        margin: 0 auto;
+    }
+
+    .news-img {
+        width: 100%;
+    }
+
+    .news-date {
+        color: #2b2b2b;
+        font-size: 50px;
+        font-weight: 900;
+        line-height: 78px;
+        transition: color .2s linear;
+    }
+
+    .news-text {
+        font-weight: 600;
+        color: #2b2b2b;
+        font-size: 16px;
+        font-style: italic;
+        line-height: 20px;
+        transition: color .2s linear;
+    }
+
+    .news-line {
+        width: 50px;
+        color: #000;
+        background: #000;
+        transition: color .2s linear,
+        background .2s linear;
+    }
+
+    .news:hover {
+        background: #000;
+    }
+
+    .news:hover .news-date {
+        color: #ffffff;
+    }
+
+    .news:hover .news-text {
+        color: #ffffff;
+    }
+
+    .news:hover .news-line {
+        color: #ffffff;
+        background: #ffffff;
+    }
 </style>
