6a4af8f11aeed5f22e66d38beb1448570a8815a5
SkyWalker1996x <chehuta1996@gmail.com>
2019-12-04 11:39:58 +0200

    upd NewsItemComponent, add router-link to NewsComponent, add data from NewsComponent

--- resources/js/components/Other/NewsItemComponent.vue
+++ resources/js/components/Other/NewsItemComponent.vue
@@ -4,16 +4,16 @@
         <section class="history-section mt-50">
             <b-container class="news-list">
                 <div class="news-item">
-                    <b-nav-item href="/news">Всі новини</b-nav-item>
+                    <router-link :to="{ name: 'news'}">Всі новини</router-link>
                     <div class="news-item-container">
-                        <img src="" alt="" class="news-item-img">
+                        <img :src="this.$route.params.img" alt="" class="news-item-img">
                         <div class="news-item-description">
-                            <h3 class="news-item-title"></h3>
-                            <h3 class="news-item-date"></h3>
+                            <h3 class="news-item-title"> {{ this.$route.params.title }}</h3>
+                            <h3 class="news-item-date"> {{ this.$route.params.date }}</h3>
                         </div>
                         <hr class="news-item-line">
                         <div class="news-item-text">
-                            <p></p>
+                            <p>{{ this.$route.params.text }}</p>
                         </div>
                         <div class="news-item-subwrapper"></div>
                     </div>
@@ -25,7 +25,7 @@
 
 <script>
     export default {
-        name: "NewsItemComponent"
+        name: "NewsItemComponent",
     }
 </script>
 
