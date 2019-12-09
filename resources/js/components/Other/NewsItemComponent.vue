ff5679d081f7ab8903fcf0358211d7819f311bab
SkyWalker1996x <chehuta1996@gmail.com>
2019-12-09 10:33:24 +0200

    upd styles

--- resources/js/components/Other/NewsItemComponent.vue
+++ resources/js/components/Other/NewsItemComponent.vue
@@ -2,19 +2,27 @@
     <div>
         <page-title title="актуальні" uptitle="новини"></page-title>
         <section class="history-section mt-50">
-            <b-container class="news-list">
+            <b-container>
+                <router-link :to="{ name: 'news'}" class="news-item-router">
+                    <img src="/img/arrow-left.png" alt="" class="news-item-router-img">
+                    <h3 class="news-item-router-title">Всі новини</h3>
+                </router-link>
+
                 <div class="news-item">
-                    <router-link :to="{ name: 'news'}">Всі новини</router-link>
                     <div class="news-item-container">
                         <img :src="this.$route.params.img" alt="" class="news-item-img">
                         <div class="news-item-description">
-                            <h3 class="news-item-title"> {{ this.$route.params.title }}</h3>
-                            <h3 class="news-item-date"> {{ this.$route.params.date }}</h3>
+                            <div class="news-item-heading">
+                                <h3 class="news-item-title"> {{ this.$route.params.title }}</h3>
+                                <h3 class="news-item-date"> {{ this.$route.params.date }}</h3>
+                            </div>
+
+                            <hr class="news-item-line">
+                            <div class="news-item-text">
+                                <p>{{ this.$route.params.text }}</p>
+                            </div>
                         </div>
-                        <hr class="news-item-line">
-                        <div class="news-item-text">
-                            <p>{{ this.$route.params.text }}</p>
-                        </div>
+
                         <div class="news-item-subwrapper"></div>
                     </div>
                 </div>
@@ -30,5 +38,89 @@
 </script>
 
 <style scoped>
+    .news-item {
+        position: relative;
+        margin-bottom: 129px;
+        display: flex;
+        flex-direction: column;
+        box-shadow: 5px 5px 25px rgba(0, 0, 0, 0.16);
+        background: #ffffff;
+    }
 
+    .news-item-router {
+        margin-bottom: 30px;
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+    }
+
+    .news-item-router-img {
+        margin-right: 24px;
+        height: -webkit-fit-content;
+        height: -moz-fit-content;
+        height: fit-content;
+    }
+
+    .news-item-router-title {
+        padding-top: 5px;
+        font-size: 32px;
+        text-decoration: none;
+        color: #2B2B2B;
+    }
+
+    .news-item-subwrapper {
+        width: 1059px;
+        height: -webkit-fill-available;
+        height: -moz-available;
+        height: fill-available;
+        background: -webkit-gradient(linear, left top, left bottom, from(#e91b47), to(#6a0017));
+        background: -o-linear-gradient(top, #e91b47 0%, #6a0017 100%);
+        background: linear-gradient(180deg, #e91b47 0%, #6a0017 100%);
+        position: absolute;
+        z-index: -100;
+        bottom: -80px;
+        right: -80px;
+    }
+
+    .news-item-img {
+        width: 100%;
+        margin-bottom: 44px;
+        height: 450px;
+    }
+
+    .news-item-description {
+        padding: 0 45px;
+        display: flex;
+        flex-direction: column;
+        justify-content: start;
+    }
+
+    .news-item-heading {
+        display: flex;
+        justify-content: space-between;
+
+    }
+    .news-item-title {
+        font-size: 42px;
+        font-weight: 800;
+        text-transform: uppercase;
+    }
+
+    .news-item-date {
+        font-size: 42px;
+        font-weight: 800;
+        text-transform: uppercase;
+    }
+
+    .news-item-line {
+        width: 50px;
+        height: 1px;
+        border-color: #2B2B2B;
+        margin-left: 0;
+    }
+
+    .news-item-text {
+        margin-bottom: 77px;
+        font-size: 18px;
+    }
 </style>
