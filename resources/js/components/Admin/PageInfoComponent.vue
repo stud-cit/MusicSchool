aa29b7b0c275553280bf3e94dfa5a96ee868fba2
doratoles <doratoles@gmail.com>
2020-02-24 17:57:07 +0400

    fix

--- resources/js/components/Admin/PageInfoComponent.vue
+++ resources/js/components/Admin/PageInfoComponent.vue
@@ -12,17 +12,21 @@
                         <div class="card-body p-2">
                             <label for="titleHistory" class="brtop">Заголовок</label>
                             <input name="titleHistory" type="text" class="form-control" v-model="data.history.title" id="titleHistory" disabled v-validate="{required: true}">
-                            <dix class="text-danger" v-if="errors.has('titleHistory')">Обов'язкове поле</dix>
+                            <div class="text-danger" v-if="errors.has('titleHistory')">Обов'язкове поле</div>
 
-                            <label for="history" class="brtop">Опис</label>
+                            <label for="shirtHistory" class="brtop">Короткий опис (має бути не більше 120 символів)</label>
+                            <input name="shirtHistory" type="text" class="form-control" v-model="data.departments.shirt_text" id="shirtHistory" maxlength="120" disabled v-validate="{required: true}">
+                            <div class="text-danger" v-if="errors.has('shirtHistory')">Обов'язкове поле</div>
+
+                            <label for="history" class="brtop">Детальний опис</label>
                             <textarea name="history" class="form-control" id="history"
                                 v-model="data.history.text" rows="4"
                                 disabled
-                                v-validate="{ required: true}"
-                                data-vv-as="Опис сторінки з історією"
+                                v-validate="{ required: false}"
+                                data-vv-as="Детальний опис сторінки з історією"
                             ></textarea>
                             <span class="errors text-danger" v-if="errors.has('history')">Обов'язкове поле</span><br>
-                            <button :disabled="errors.has('history') || errors.has('titleHistory')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "history", "titleHistory")'>Редагувати</button><br><br>
+                            <button :disabled="errors.has('history') || errors.has('titleHistory')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "history", "shirtHistory", "titleHistory")'>Редагувати</button><br><br>
                         </div>
                     </div>
                     <br>
@@ -35,15 +39,19 @@
                             <input name="titleNews" type="text" class="form-control" v-model="data.news.title" id="titleNews" disabled v-validate="{required: true}">
                             <div class="text-danger" v-if="errors.has('titleNews')">Обов'язкове поле</div>
 
-                            <label for="news" class="brtop">Опис</label>
+                            <label for="shirtNews" class="brtop">Короткий опис (має бути не більше 120 символів)</label>
+                            <input name="shirtNews" type="text" class="form-control" v-model="data.departments.shirt_text" id="shirtNews" maxlength="120" disabled v-validate="{required: true}">
+                            <div class="text-danger" v-if="errors.has('shirtNews')">Обов'язкове поле</div>
+
+                            <label for="news" class="brtop">Детальний опис</label>
                             <textarea name="news" class="form-control" id="news"
                                 v-model="data.news.text" rows="4"
                                 disabled
-                                v-validate="{ required: true}"
-                                data-vv-as="Опис сторінки з новинами"
+                                v-validate="{ required: false}"
+                                data-vv-as="Детальний опис сторінки з новинами"
                             ></textarea>
                             <span class="errors text-danger" v-if="errors.has('news')">Обов'язкове поле</span><br>
-                            <button :disabled="errors.has('news') || errors.has('titleNews')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "news", "titleNews")'>Редагувати</button><br><br>
+                            <button :disabled="errors.has('news') || errors.has('titleNews')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "news", "shirtNews", "titleNews")'>Редагувати</button><br><br>
                         </div>
                     </div>
                     <br>
@@ -56,8 +64,8 @@
                             <input name="titleDepartments" type="text" class="form-control" v-model="data.departments.title" id="titleDepartments" disabled v-validate="{required: true}">
                             <div class="text-danger" v-if="errors.has('titleDepartments')">Обов'язкове поле</div>
 
-                            <label for="shirtDepartments" class="brtop">Короткий опис</label>
-                            <input name="shirtDepartments" type="text" class="form-control" v-model="data.departments.shirt_text" id="shirtDepartments" disabled v-validate="{required: true}">
+                            <label for="shirtDepartments" class="brtop">Короткий опис (має бути не більше 120 символів)</label>
+                            <input name="shirtDepartments" type="text" class="form-control" v-model="data.departments.shirt_text" id="shirtDepartments" maxlength="120" disabled v-validate="{required: true}">
                             <div class="text-danger" v-if="errors.has('shirtDepartments')">Обов'язкове поле</div>
 
                             <label for="departments" class="brtop">Детальний опис</label>
@@ -81,16 +89,19 @@
                             <input name="titleAchievements" type="text" class="form-control" v-model="data.achievements.title" id="titleAchievements" disabled v-validate="{required: true}">
                             <div class="text-danger" v-if="errors.has('titleAchievements')">Обов'язкове поле</div>
 
-                            <label for="achievements" class="brtop">Опис (має бути не більше 120 символів)</label>
+                            <label for="shirtAchievements" class="brtop">Короткий опис (має бути не більше 120 символів)</label>
+                            <input name="shirtAchievements" type="text" class="form-control" v-model="data.departments.shirt_text" id="shirtAchievements" maxlength="120" disabled v-validate="{required: true}">
+                            <div class="text-danger" v-if="errors.has('shirtAchievements')">Обов'язкове поле</div>
+
+                            <label for="achievements" class="brtop">Детальний опис</label>
                             <textarea name="achievements" class="form-control" id="achievements"
                                 v-model="data.achievements.text" rows="4"
                                 disabled
-                                maxlength="120"
-                                v-validate="{ required: true}"
-                                data-vv-as="Опис сторінки з досягненнями"
+                                v-validate="{ required: false}"
+                                data-vv-as="Детальний опис сторінки з досягненнями"
                             ></textarea>
                             <span class="errors text-danger" v-if="errors.has('achievements')">Обов'язкове поле</span><br>
-                            <button :disabled="errors.has('achievements') || errors.has('titleAchievements')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "achievements", "titleAchievements")'>Редагувати</button><br><br>
+                            <button :disabled="errors.has('achievements') || errors.has('titleAchievements')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "achievements", "shirtAchievements", "titleAchievements")'>Редагувати</button><br><br>
                         </div>
                     </div>
                 </div>
@@ -104,16 +115,20 @@
                             <label for="titleIntroduction" class="brtop">Заголовок</label>
                             <input name="titleIntroduction" type="text" class="form-control" v-model="data.introduction.title" id="titleIntroduction" disabled v-validate="{required: true}">
                             <div class="text-danger" v-if="errors.has('titleIntroduction')">Обов'язкове поле</div>
+                            
+                            <label for="shirtIntroduction" class="brtop">Короткий опис (має бути не більше 120 символів)</label>
+                            <input name="shirtIntroduction" type="text" class="form-control" v-model="data.departments.shirt_text" id="shirtIntroduction" maxlength="120" disabled v-validate="{required: true}">
+                            <div class="text-danger" v-if="errors.has('shirtIntroduction')">Обов'язкове поле</div>
 
-                            <label for="introduction" class="brtop">Опис</label>
+                            <label for="introduction" class="brtop">Детальний опис</label>
                             <textarea name="introduction" class="form-control" id="introduction"
                                 v-model="data.introduction.text" rows="4"
                                 disabled
-                                v-validate="{ required: true}"
-                                data-vv-as="Опис сторінки вступу"
+                                v-validate="{ required: false}"
+                                data-vv-as="Детальний опис сторінки вступу"
                             ></textarea>
                             <span class="errors text-danger" v-if="errors.has('introduction')">Обов'язкове поле</span><br>
-                            <button :disabled="errors.has('introduction') || errors.has('titleIntroduction')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "introduction", "titleIntroduction")'>Редагувати</button><br><br>
+                            <button :disabled="errors.has('introduction') || errors.has('titleIntroduction')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "introduction", "shirtIntroduction", "titleIntroduction")'>Редагувати</button><br><br>
                         </div>
                     </div>
                     <br>
@@ -126,15 +141,19 @@
                             <input name="titleGallery" type="text" class="form-control" v-model="data.gallery.title" id="titleGallery" disabled v-validate="{required: true}">
                             <div class="text-danger" v-if="errors.has('titleGallery')">Обов'язкове поле</div>
 
-                            <label for="gallery" class="brtop">Опис</label>
+                            <label for="shirtGallery" class="brtop">Короткий опис (має бути не більше 120 символів)</label>
+                            <input name="shirtGallery" type="text" class="form-control" v-model="data.departments.shirt_text" id="shirtGallery" maxlength="120" disabled v-validate="{required: true}">
+                            <div class="text-danger" v-if="errors.has('shirtGallery')">Обов'язкове поле</div>
+
+                            <label for="gallery" class="brtop">Детальний опис</label>
                             <textarea name="gallery" class="form-control" id="gallery"
                                 v-model="data.gallery.text" rows="4"
                                 disabled
-                                v-validate="{ required: true}"
-                                data-vv-as="Опис головної сторінки"
+                                v-validate="{ required: false}"
+                                data-vv-as="Детальний опис головної сторінки"
                             ></textarea>
                             <span class="errors text-danger" v-if="errors.has('gallery')">Обов'язкове поле</span><br>
-                            <button :disabled="errors.has('gallery') || errors.has('titleGallery')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "gallery", "titleGallery")'>Редагувати</button><br><br>
+                            <button :disabled="errors.has('gallery') || errors.has('titleGallery')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "gallery", "shirtGallery", "titleGallery")'>Редагувати</button><br><br>
                         </div>
                     </div>
                     <br>
@@ -147,15 +166,19 @@
                             <input name="titleСontacts" type="text" class="form-control" v-model="data.contacts.title" id="titleСontacts" disabled v-validate="{required: true}">
                             <div class="text-danger" v-if="errors.has('titleСontacts')">Обов'язкове поле</div>
 
-                            <label for="contacts" class="brtop">Опис</label>
+                            <label for="shirtСontacts" class="brtop">Короткий опис (має бути не більше 120 символів)</label>
+                            <input name="shirtСontacts" type="text" class="form-control" v-model="data.departments.shirt_text" id="shirtСontacts" maxlength="120" disabled v-validate="{required: true}">
+                            <div class="text-danger" v-if="errors.has('shirtСontacts')">Обов'язкове поле</div>
+
+                            <label for="contacts" class="brtop">Детальний опис</label>
                             <textarea name="contacts" class="form-control" id="contacts"
                                 v-model="data.contacts.text" rows="4"
                                 disabled
-                                v-validate="{ required: true}"
-                                data-vv-as="Опис сторінки з історією"
+                                v-validate="{ required: false}"
+                                data-vv-as="Детальний опис сторінки з історією"
                             ></textarea>
                             <span class="errors text-danger" v-if="errors.has('contacts')">Обов'язкове поле</span><br>
-                            <button :disabled="errors.has('contacts') || errors.has('titleСontacts')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "contacts", "titleСontacts")'>Редагувати</button><br><br>
+                            <button :disabled="errors.has('contacts') || errors.has('titleСontacts')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "contacts", "shirtСontacts", "titleСontacts")'>Редагувати</button><br><br>
                         </div>
                     </div>
                     <br>
@@ -168,15 +191,19 @@
                             <input name="titleTeachers" type="text" class="form-control" v-model="data.teachers.title" id="titleTeachers" disabled v-validate="{required: true}">
                             <div class="text-danger" v-if="errors.has('titleTeachers')">Обов'язкове поле</div>
 
-                            <label for="teachers" class="brtop">Опис</label>
+                            <label for="shirtTeachers" class="brtop">Короткий опис (має бути не більше 120 символів)</label>
+                            <input name="shirtTeachers" type="text" class="form-control" v-model="data.departments.shirt_text" id="shirtTeachers" maxlength="120" disabled v-validate="{required: true}">
+                            <div class="text-danger" v-if="errors.has('shirtTeachers')">Обов'язкове поле</div>
+
+                            <label for="teachers" class="brtop">Детальний опис</label>
                             <textarea name="teachers" class="form-control" id="teachers"
                                 v-model="data.teachers.text" rows="4"
                                 disabled
-                                v-validate="{ required: true}"
-                                data-vv-as="Опис сторінки з викладачами"
+                                v-validate="{ required: false}"
+                                data-vv-as="Детальний опис сторінки з викладачами"
                             ></textarea>
                             <span class="errors text-danger" v-if="errors.has('teachers')">Обов'язкове поле</span><br>
-                            <button :disabled="errors.has('teachers') || errors.has('titleTeachers')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "teachers", "titleTeachers")'>Редагувати</button><br><br>
+                            <button :disabled="errors.has('teachers') || errors.has('titleTeachers')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "teachers", "shirtTeachers", "titleTeachers")'>Редагувати</button><br><br>
                         </div>
                     </div>
                     <br>
@@ -189,15 +216,19 @@
                             <input name="titleInstruments" type="text" class="form-control" v-model="data.instruments.title" id="titleInstruments" disabled v-validate="{required: true}">
                             <div class="text-danger" v-if="errors.has('titleInstruments')">Обов'язкове поле</div>
 
-                            <label for="instruments" class="brtop">Опис</label>
+                            <label for="shirtInstruments" class="brtop">Короткий опис (має бути не більше 120 символів)</label>
+                            <input name="shirtInstruments" type="text" class="form-control" v-model="data.departments.shirt_text" id="shirtInstruments" maxlength="120" disabled v-validate="{required: true}">
+                            <div class="text-danger" v-if="errors.has('shirtInstruments')">Обов'язкове поле</div>
+
+                            <label for="instruments" class="brtop">Детальний опис</label>
                             <textarea name="instruments" class="form-control" id="instruments"
                                 v-model="data.instruments.text" rows="4"
                                 disabled
-                                v-validate="{ required: true}"
-                                data-vv-as="Опис сторінки з інструментами"
+                                v-validate="{ required: false}"
+                                data-vv-as="Детальний опис сторінки з інструментами"
                             ></textarea>
                             <span class="errors text-danger" v-if="errors.has('instruments')">Обов'язкове поле</span><br>
-                            <button :disabled="errors.has('instruments') || errors.has('titleInstruments')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "instruments", "titleInstruments")'>Редагувати</button><br><br>
+                            <button :disabled="errors.has('instruments') || errors.has('titleInstruments')" type="button" class="btn btn-outline-secondary my-2 px-5 float-right edit" @click='edit($event, "instruments", "shirtInstruments", "titleInstruments")'>Редагувати</button><br><br>
                         </div>
                     </div>
                 </div>
